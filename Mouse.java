14e0fd69b072f1274ed4ed886a891cebe3355bb4
Livia Menezes <liviasbm@hotmail.com>
2022-10-01 14:45:14 -0400

    Update Mouse.java
    Code modified - Mouse qty to be automatically generated. Range: 1-25

--- Mouse.java
+++ Mouse.java
@@ -1,6 +1,7 @@
 //Student Name: Livia Menezes
 //Student ID: 261066016
 import java.util.Scanner;
+import java.util.Random;
 
 public class Mouse {
 
@@ -10,9 +11,18 @@ public class Mouse {
 
 
     public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);
-            System.out.println("Inform the inicial number of mouses:");
-            int qty_mouses = myObj.nextInt();
+    //    Scanner myObj = new Scanner(System.in);
+    //        System.out.println("Inform the inicial number of mouses:");
+    //        int qty_mouses = myObj.nextInt();
+        
+        Random rand = new Random(); //instance of random class
+        int lowerbound = 1;
+        int upperbound = 25; //generate random values from 1-25
+              
+        int qty_mouses = rand.nextInt(upperbound-lowerbound+1);
+        System.out.println(qty_mouses);
+
+
         String[] myArr = new String[qty_mouses];
 
         for (int a=0; a<qty_mouses; a++){
