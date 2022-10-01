1ff738a0440c17ab97148d82626deff082b5b3b9
Livia Menezes <liviasbm@hotmail.com>
2022-10-01 14:53:29 -0400

    Update Mouse.java
    Initial quantity of mouses, traps and cheese set

--- Mouse.java
+++ Mouse.java
@@ -11,23 +11,25 @@ public class Mouse {
 
 
     public static void main(String[] args) {
-    //    Scanner myObj = new Scanner(System.in);
-    //        System.out.println("Inform the inicial number of mouses:");
-    //        int qty_mouses = myObj.nextInt();
-        
         Random rand = new Random(); //instance of random class
-        int lowerbound = 1;
-        int upperbound = 25; //generate random values from 1-25
+        int min = 1;
+        int max_mouses = 15; //generate random values from 1-25
               
-        int qty_mouses = rand.nextInt(upperbound-lowerbound+1);
-        System.out.println(qty_mouses);
+        int x = rand.nextInt(max_mouses-min+1);
+        System.out.println("Quantity of mouses: "+x);
 
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter the quantity of traps you'd like to try: ");
+        int y = myObj.nextInt();
+        //System.out.println("Quantity of traps: "+y);
+        int cheese_qty = 10;
+        //System.out.println("Initial quantity of cheese: "+cheese_qty+" grams.");
 
-        String[] myArr = new String[qty_mouses];
+        String[] myArr = new String[x];
 
-        for (int a=0; a<qty_mouses; a++){
+        for (int a=0; a<x; a++){
             myArr[a]="mouse-"+(a+1);
-            System.out.println(myArr[a]);
+            //System.out.println(myArr[a]);
         }
         
     }
